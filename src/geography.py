f7efe738b482298e580fef13dc74333634ccc4ae
charlieoneill11 <cponeill00@gmail.com>
2023-02-28 21:53:34 +1100

    geography class

---
+++ src/geography.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+class Geography:
+    def __init__(self, city_name, grid_size):
+        self.city_name = city_name
+        self.grid_size = grid_size
+        self.topology = self.generate_topology()
+        self.household_density = self.generate_household_density()
+    
+    def generate_topology(self):
+        # Generate topology using some method (e.g., a terrain map)
+        return np.random.rand(self.grid_size, self.grid_size)
+    
+    def generate_household_density(self):
+        # Load household data from file or database
+        # Compute household density based on location
+        # For example, using a kernel density estimator
+        return np.random.rand(self.grid_size, self.grid_size)
+    
+    def visualise_topology(self):
+        # Visualize the topology using a 3D plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        x = np.arange(0, self.grid_size, 1)
+        y = np.arange(0, self.grid_size, 1)
+        X, Y = np.meshgrid(x, y)
+        ax.plot_surface(X, Y, self.topology, cmap='terrain')
+        plt.show()
+    
+    def visualise_household_density(self):
+        # Visualize the household density using a 2D plot
+        plt.imshow(self.household_density, cmap='Blues', origin='lower')
+        plt.colorbar()
+        plt.show()
+    
+    def visualise_combined(self):
+        # Visualize the combination of topology and household density using a 2D heat map
+        plt.imshow(self.topology * self.household_density, cmap='inferno', origin='lower')
+        plt.colorbar()
+        plt.show()
+
+geo = Geography("City A", 100)
+geo.visualise_topology()
+geo.visualise_household_density()
+geo.visualise_combined()
